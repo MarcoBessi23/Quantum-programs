7a058ee362417a9489628835c045beed0043fa68
MarcoBessi23 <marcobessi23@gmail.com>
2025-02-22 16:03:58 +0100

    first_commit

---
+++ grover.py
@@ -0,0 +1,71 @@
+import numpy as np
+import math
+# Import Qiskit
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+from qiskit_aer.primitives import SamplerV2
+from qiskit.visualization import plot_histogram, plot_state_city
+import qiskit.quantum_info as qi# Numero di qubit
+import matplotlib.pyplot as plt
+
+
+n = 3
+qc = QuantumCircuit(n)
+
+# Step 1: Inizializzo con Hadamard tutti i qubit
+qc.h([0, 1, 2])
+
+def oracle(qc):
+    qc.x(1)       #porta 101 in 111
+    qc.h(2)
+    qc.ccx(0, 1, 2)  # Toffoli su |101> (trasformato in |111>)
+    qc.h(2)
+    qc.x(1)       #riporta qubit in 0
+    
+
+# Operatore di diffusione (inversione rispetto alla media)
+def diffuser(qc):
+    qc.h([0, 1, 2])  # Hadamard su tutti i qubit
+    qc.x([0, 1, 2])  # NOT su tutti i qubit
+    qc.h(2)          # Hadamard sull'ultimo qubit
+    qc.ccx(0, 1, 2)  # Toffoli
+    qc.h(2)          # Hadamard per chiudere l'operazione
+    qc.x([0, 1, 2])  # NOT su tutti i qubit
+    qc.h([0, 1, 2])  # Hadamard su tutti i qubit
+
+
+#numero iterazioni = (pi/4)*sqrt(N/m) vale solo quando N è molto maggiore del numero delle soluzioni m, 
+# quindi quando theta è molto piccolo
+# Iterazione dell'algoritmo di Grover (2 iterazioni)
+oracle(qc)      # Prima iterazione dell'oracolo
+diffuser(qc)    # Prima iterazione del diffusore
+
+
+# Misurazione
+qc.measure_all()
+#qc.draw(output="mpl", style="iqp")
+
+
+# Transpile for simulator
+simulator = AerSimulator()
+circ = transpile(qc, simulator)
+
+## Run and get counts
+result = simulator.run(circ).result()
+counts = result.get_counts(circ)
+
+plot_histogram(counts)
+plt.show()
+
+
+
+
+
+## Construct an ideal simulator with SamplerV2
+sampler = SamplerV2()
+job = sampler.run([circ], shots=1024)
+
+# Perform an ideal simulation
+result_ideal = job.result()
+counts_ideal = result_ideal[0].data.meas.get_counts()
+print('Counts(ideal):', counts_ideal)
---
+++ grover_deutsch.py
@@ -0,0 +1,56 @@
+import numpy as np
+import math
+# Import Qiskit
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+from qiskit_aer.primitives import SamplerV2
+from qiskit.visualization import plot_histogram, plot_state_city
+import qiskit.quantum_info as qi# Numero di qubit
+import matplotlib.pyplot as plt
+
+
+n = 4
+qc = QuantumCircuit(n)
+
+#010->1
+#101->1
+#011->1
+#110->1 
+#Quindi dalle slides per fare la funzione devo costruire un circuito per cui se per esempio entra 110y 
+# allora deve uscire 110(NOTy)
+
+
+def Uf(qc):
+    #101
+    qc.x(1) #qui ho 111
+    qc.cx(0,3) #-y
+    qc.cx(1,3) #y
+    qc.cx(2,3) #-y 
+    qc.x(1) #riporto tutto nello stato di partenza
+
+    #011
+    qc.x(0)
+    qc.cx(0,3) #-y
+    qc.cx(1,3) #y
+    qc.cx(2,3) #-y 
+    qc.x(0) #riporto tutto nello stato di partenza
+
+
+
+zero_f = QuantumCircuit(1)
+
+one_f = QuantumCircuit(1)
+one_f.x(0)
+
+# Step 1: Inizializzo con Hadamard i primi tre qubit
+qc.h([0, 1, 2])
+qc.x(4) #porto il primo qubit |0> in |1>
+qc.h(4) #applico Hadamard a |1> 
+Uf(qc)
+qc.h([0, 1, 2, 3])
+
+def oracle(qc):
+    pass
+
+def diffuser(qc):
+    pass
