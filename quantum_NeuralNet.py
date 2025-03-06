b9eba03460d112e42525a3c0f28ce1e9befd286b
MarcoBessi23 <marcobessi23@gmail.com>
2025-03-06 16:13:29 +0100

    tentative with estimatorQNN

--- quantum_NeuralNet.py
+++ quantum_NeuralNet.py
@@ -1,8 +1,8 @@
 import numpy as np
 from qiskit.circuit import ParameterVector
-from qiskit.circuit.library import StatePreparation
+from qiskit.circuit.library import StatePreparation, RealAmplitudes
 from qiskit import QuantumCircuit
-
+from qiskit.circuit import QuantumCircuit, QuantumRegister
 ##BUILD THE NEURAL NETWORK
 def amplitude_encoding(input_vector:np.array):
     '''
@@ -41,77 +41,170 @@ def amplitude_encoding(input_vector:np.array):
 
     return qc
 
-def control_lambda(control_qubits:str):
-    xcir = QuantumCircuit(1)
-    xcir.x(0)
-    xgate = xcir.to_gate(label='X').control(3, ctrl_state= control_qubits)
+
+def oracle_gate_000():
+    """Oracolo che inverte la fase di |000>"""
+    oracle = QuantumCircuit(3, name="Oracle")
+    oracle.x(0)
+    oracle.x(1)
+    oracle.x(2)
+    oracle.h(2)
+    oracle.ccx(0, 1, 2)
+    oracle.h(2)
+    oracle.x(2)
+    oracle.x(1)
+    oracle.x(0)
+
+    return oracle.to_gate()
+
+
+def oracle_gate_001():
+    """Oracolo che inverte la fase di |001>"""
+    oracle = QuantumCircuit(3, name="Oracle")
+    oracle.x(0)
+    oracle.x(1)
+    oracle.h(2)
+    oracle.ccx(0, 1, 2)
+    oracle.h(2)
+    oracle.x(1)
+    oracle.x(0)
+
+    return oracle.to_gate()
+
+
+def oracle_gate_010():
+    """Oracolo che inverte la fase di |010>"""
+    oracle = QuantumCircuit(3, name="Oracle")
+    oracle.x(0)
+    oracle.x(2)
+    oracle.h(2)
+    oracle.ccx(0, 1, 2)
+    oracle.h(2)
+    oracle.x(2)
+    oracle.x(0)
+
+    return oracle.to_gate()
+
+def oracle_gate_011():
+    """Oracolo che inverte la fase di |011>"""
+    oracle = QuantumCircuit(3, name="Oracle")
+    oracle.x(0)
+    oracle.h(2)
+    oracle.ccx(0, 1, 2)
+    oracle.h(2)
+    oracle.x(0)
+
+    return oracle.to_gate()
+
+
+def oracle_gate_100():
+    """Oracolo che inverte la fase di |100>"""
+    oracle = QuantumCircuit(3, name="Oracle")
+    oracle.x(1)
+    oracle.x(2)
+    oracle.h(2)
+    oracle.ccx(0, 1, 2)
+    oracle.h(2)
+    oracle.x(2)
+    oracle.x(1)
     
-    return xgate
+    return oracle.to_gate()
+
+def oracle_gate_101():
+    """Oracolo che inverte la fase di |101>"""
+    oracle = QuantumCircuit(3, name="Oracle")
+    oracle.x(1)
+    oracle.h(2)
+    oracle.ccx(0, 1, 2)
+    oracle.h(2)
+    oracle.x(1)
+    
+    return oracle.to_gate()
+
+def oracle_gate_110():
+    """Oracolo che inverte la fase di |110>"""
+    oracle = QuantumCircuit(3, name="Oracle")
+    #oracle.x(0)
+    #oracle.x(1)
+    oracle.x(2)
+    oracle.h(2)
+    oracle.ccx(0, 1, 2)
+    oracle.h(2)
+    oracle.x(2)
+    #oracle.x(1)
+    #oracle.x(0)
+
+    return oracle.to_gate()
+
+def oracle_gate_111():
+    """Oracolo che inverte la fase di |111>"""
+    oracle = QuantumCircuit(3, name="Oracle")
+    oracle.h(2)
+    oracle.ccx(0, 1, 2)
+    oracle.h(2)
+    
+    return oracle.to_gate()
+
+def controlled_oracle(qbit_str, oracle_gate):
+    """Oracolo controllato per |100>"""
+    control = QuantumRegister(1, "control")  # Qubit di controllo
+    target = QuantumRegister(3, "target")    # Qubits per l'oracolo
+    qc = QuantumCircuit(control, target)
+
+    oracle = oracle_gate()
+    controlled_oracle = oracle.control(1)
+
+    qc.append(controlled_oracle, [control[0], target[0], target[1], target[2]])
+    return qc.to_gate(label=f"ControlledOracle{qbit_str}]")
+
+
+controlled_gate_000 = controlled_oracle('000', oracle_gate_000)
+controlled_gate_001 = controlled_oracle('001', oracle_gate_001)
+controlled_gate_010 = controlled_oracle('010', oracle_gate_010)
+controlled_gate_011 = controlled_oracle('011', oracle_gate_011)
+controlled_gate_100 = controlled_oracle('100', oracle_gate_100)
+controlled_gate_101 = controlled_oracle('101', oracle_gate_101)
+controlled_gate_110 = controlled_oracle('110', oracle_gate_110)
+controlled_gate_111 = controlled_oracle('111', oracle_gate_111)
+
+
 
 #https://github.com/Qiskit/textbook/blob/main/notebooks/ch-algorithms/grover.ipynb
 def flexible_oracle(qc):
+
     theta = ParameterVector('theta', length = 8)
 
     #|000>
     qc.rx(theta[0], 0)
-    qc.x(3)#put third bit in 1, same idea as for grover exercise in grover.py 
-    qc.h(3)
-    control0 = control_lambda('001')    
-    qc.append(control0, [0,1,2,3])
-    qc.h(3)
-    qc.x(3)
+    qc.append(controlled_gate_000, [0,1,2,3])
 
     #|001>
     qc.rx(theta[1], 0)
-    qc.h(3)
-    qc.append(control0, [0,1,2,3])
-    qc.h(3)
+    qc.append(controlled_gate_001, [0,1,2,3])
 
     #|010>
     qc.rx(theta[2], 0)
-    
-    qc.x(3)
-    qc.h(3)
-    control2 = control_lambda('101')    
-    qc.append(control2, [0,1,2,3])
-    qc.h(3)
-    qc.x(3)
+    qc.append(controlled_gate_010, [0,1,2,3])
     
     #|011>
     qc.rx(theta[3], 0)
-    qc.h(3)
-    qc.append(control2, [0,1,2,3])
-    qc.h(3)
-    
+    qc.append(controlled_gate_011, [0,1,2,3])
+
     #|100>
     qc.rx(theta[4], 0)
-    qc.x(3)
-    qc.h(3)
-    control3 = control_lambda('011')    
-    qc.append(control3, [0,1,2,3])
-    qc.h(3)
-    qc.x(3)
-
+    qc.append(controlled_gate_100, [0,1,2,3])
+    
     #|101>
     qc.rx(theta[5],0)
-    qc.h(3)
-    qc.append(control3, [0,1,2,3])
-    qc.h(3)
-
+    qc.append(controlled_gate_101, [0,1,2,3])
+    
     #|110>
     qc.rx(theta[6],0)
-    qc.x(3)
-    qc.h(3)
-    control4 = control_lambda('111')
-    qc.append(control4, [0,1,2,3])
-    qc.h(3)
-    qc.x(3)
-
+    qc.append(controlled_gate_110, [0,1,2,3])
+    
     #|111>
     qc.rx(theta[7],0)
-    qc.h(3)
-    qc.append(control4, [0,1,2,3])
-    qc.h(3)
+    qc.append(controlled_gate_111, [0,1,2,3])
 
     return qc
 
@@ -121,22 +214,23 @@ def adaptive_diffusion(qc):
     '''
 
     ### FIRST COSTRUCT THE MULTICONTROL ZGATE TO USE IN THE NETWORK
-    zcir = QuantumCircuit(1)
-    zcir.z(0)
-    zgate = zcir.to_gate(label='z').control(3, ctrl_state= '111')
-
+    #zcir = QuantumCircuit(1)
+    #zcir.z(0)
+    #zgate = zcir.to_gate(label='z').control(3, ctrl_state= '111')
 
     psi = ParameterVector('psi', length = 6)
     qc.h([1,2,3])
     qc.cry(psi[0], 1, 2)
     qc.cry(psi[1], 2, 3)
     qc.cry(psi[2], 3, 1)
-    ###append the multicontrol Z gate
-    qc.append(zgate, [0,1,2,3])
+    ##MULTI CONTROLLED Z GATE
+    qc.h(3)
+    qc.ccx(1, 2, 3)
+    qc.h(3)
     qc.cry(psi[3], 3, 1)
     qc.cry(psi[4], 2, 3)
     qc.cry(psi[5], 1, 2)
-
+    qc.barrier()
     qc.h([1,2,3])
 
     return qc
@@ -146,7 +240,7 @@ def GQHAN(input):
     Grover inspired Quantum Attention Network from https://arxiv.org/abs/2401.14089
     '''
     
-    qc = QuantumCircuit(4)#,3)
+    qc = QuantumCircuit(4)
     initial_state = input/np.linalg.norm(input)
     amplitude = StatePreparation(initial_state) ##Amplitude Encoding
     qc.append(amplitude, [1,2,3])
@@ -154,3 +248,59 @@ def GQHAN(input):
     qc = adaptive_diffusion(qc)
 
     return qc
+
+
+def ansatz(qc):
+    qc = flexible_oracle(qc)
+    qc = adaptive_diffusion(qc)
+    return qc
+
+#def GQHAN():
+#    '''
+#    Grover inspired Quantum Attention Network from https://arxiv.org/abs/2401.14089
+#    '''
+#    
+#    qc = QuantumCircuit(4) #il primo qubit serve per altre applicazioni, usare solo 1,2,3
+#    x = ParameterVector('x', length = 8)
+#    amplitude = StatePreparation(x) ##Amplitude Encoding
+#    qc.append(amplitude, [1,2,3])
+#    qc = flexible_oracle(qc)
+#    qc = adaptive_diffusion(qc)
+#
+#    return qc
+
+
+
+def diffuser(qc):
+    qc.h([1, 2, 3])  # Hadamard su tutti i qubit
+    qc.x([1, 2, 3])  # NOT su tutti i qubit
+    qc.h(3)          # Hadamard sull'ultimo qubit
+    qc.ccx(1, 2, 3)  # Toffoli
+    qc.h(3)          # Hadamard per chiudere l'operazione
+    qc.x([1, 2, 3])  # NOT su tutti i qubit
+    qc.h([1, 2, 3])  # Hadamard su tutti i qubit
+
+#qc = GQHAN()
+#print(qc.draw('text'))
+
+#qc_test = QuantumCircuit(4)  # Deve avere il numero corretto di qubit
+#qc_test.x(0)
+#qc_test.h([1,2,3])
+#qc_test.append(controlled_gate_111, [0, 1, 2, 3])
+#diffuser(qc_test)
+#qc_test.measure_all()
+#
+#from qiskit import QuantumCircuit, transpile
+#from qiskit_aer import AerSimulator
+## Transpile for simulator
+#simulator = AerSimulator()
+#circ = transpile(qc_test, simulator)
+#
+### Run and get counts
+#result = simulator.run(circ).result()
+#counts = result.get_counts(circ)
+#
+#import matplotlib.pyplot as plt
+#from qiskit.visualization import plot_histogram
+#plot_histogram(counts)
+#plt.show()
--- train.py
+++ train.py
@@ -1,63 +1,41 @@
 import os
 import numpy as np
 from qiskit.circuit import ParameterVector, Parameter
-from qiskit_algorithms.gradients import ParamShiftSamplerGradient, ParamShiftEstimatorGradient
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Pauli
-from qiskit.primitives import Estimator
+from qiskit.primitives import Estimator, StatevectorEstimator
 from data_preprocessing import PCA_data
 from quantum_NeuralNet import GQHAN
+from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier, VQC
+from qiskit_machine_learning.neural_networks import EstimatorQNN
+from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier
 
 train_images_pca, test_images_pca, train_labels, test_labels = PCA_data()
 train_labels[train_labels == 0] = -1
 test_labels[test_labels == 0] = -1
 
 
-
-z_op = Pauli("ZIII")
-print(z_op)
-
-def accuracy(params, input_data, labels):
-    y_pred = []
-    for i, x in enumerate(input_data):
-        
-        qc = GQHAN(x)
-        z_op = Pauli("ZIII") ###see https://docs.quantum.ibm.com/api/qiskit/qiskit.quantum_info.Pauli
-        estimator = Estimator()
-        job = estimator.run([qc], [z_op], [params])
-        expectation_value = job.result().values[0]
-        y_pred.append(np.sign(expectation_value))
-    
-    acc = np.sum(labels == y_pred)/len(labels)
-    return acc
-
-
-
-def cost_function(params, batch, labels):
-    '''
-    Calculate loss function as shown in https://arxiv.org/abs/2401.14089    
-    '''
-    first_derivative = 0
-    total_cost = 0
-    for i, x in enumerate(batch):
-        
-        qc = GQHAN(x)
-        z_op = Pauli("ZIII")
-        estimator = Estimator()
-        job = estimator.run([qc], [z_op], [params])
-        expectation_value = job.result().values[0]
-        
-        y = labels[i]
-        #Binary Cross-Entropy
-        total_cost += (y-np.sign(expectation_value))**2
-        first_derivative += (y-np.sign(expectation_value))
-    
-    return total_cost / len(batch), -2 * first_derivative / len(batch)
-
-#def dL_df(params, batch, labels):
+#def accuracy(params, input_data, labels):
+#    y_pred = []
+#    for i, x in enumerate(input_data):
+#        
+#        qc = GQHAN(x)
+#        z_op = Pauli("ZIII") ###see https://docs.quantum.ibm.com/api/qiskit/qiskit.quantum_info.Pauli
+#        estimator = Estimator()
+#        job = estimator.run([qc], [z_op], [params])
+#        expectation_value = job.result().values[0]
+#        y_pred.append(np.sign(expectation_value))
+#    
+#    acc = np.sum(labels == y_pred)/len(labels)
+#    return acc
+#
+#
+#
+#def cost_function(params, batch, labels):
 #    '''
-#    Calculate first part of gradient    
+#    Calculate loss function as shown in https://arxiv.org/abs/2401.14089    
 #    '''
+#    dL_df = []
 #    total_cost = 0
 #    for i, x in enumerate(batch):
 #        
@@ -69,68 +47,131 @@ def cost_function(params, batch, labels):
 #        
 #        y = labels[i]
 #        #Binary Cross-Entropy
-#        total_cost += (y-np.sign(expectation_value))
-#        
-#    return -2 * total_cost / len(batch)
+#        #total_cost += (y-np.sign(expectation_value))**2
+#        total_cost += (y-expectation_value)**2
+#        #print('EXPECTATION VALUE')
+#        #print(np.sign(expectation_value))
+#        #first_derivative = y-np.sign(expectation_value)
+#        first_derivative = y-expectation_value
+#        dL_df.append(first_derivative)
+#    return total_cost / len(batch), -2 * np.array(dL_df) 
 #
+#def compute_gradient(params:np.ndarray, batch, first_derivative):
+#    '''
+#    compute gradient of the quantum part of the loss using parameter shift
+#    '''
+#
+#    estimator = Estimator()
+#    z_op = Pauli("ZIII")
+#    grad = np.zeros_like(params)
+#    print('Gradient calculation')
+#    for i, x in enumerate(batch):
+#        qc = GQHAN(x)
+#        gradient = SPSAEstimatorGradient(estimator, epsilon=0.05)
+#        pse_grad_result = gradient.run(qc, z_op, [params.tolist()]).result().gradients
+#        g = np.array(pse_grad_result)[0]
+#        g *= first_derivative[i]
+#        grad += g
+#        #print('GRADIENT VALUE')
+#        #print(g)
+#        
+#    return grad/len(batch)
 
-def compute_gradient(params:np.ndarray, batch):
-    '''
-    compute gradient of the quantum part of the loss using parameter shift
-    '''
+#from qiskit_algorithms.gradients.utils import DerivativeType
+#
+#def compute_gradient(params:np.ndarray, batch, first_derivative):
+#    '''
+#    compute gradient of the quantum part of the loss using parameter shift
+#    '''
+#
+#    
+#    
+#    z_op = Pauli("ZIII")
+#    grad = np.zeros_like(params)
+#    print('Gradient calculation')
+#    for i, x in enumerate(batch):
+#        qc = GQHAN(x)
+#        estimator = StatevectorEstimator()
+#        gradient = ReverseQGT(estimator, derivative_type=DerivativeType.REAL)
+#        pse_grad_result = gradient.run(qc,[params.tolist()]).result().gradients
+#        g = np.array(pse_grad_result)[0]
+#        g *= first_derivative[i]
+#        grad += g
+#        #print('GRADIENT VALUE')
+#        #print(g)
+#        
+#    return grad/len(batch)
+#
+#
+##init_params = np.random.uniform(0, 2*np.pi, 14)
+##init_params = init_params.astype(np.float64)
+##batch = train_images_pca[0 : 30]
+##batch_labels = train_labels[0 : 30]
+#first_derivative = np.ones((30,))
+#compute_gradient(init_params, batch, first_derivative)
 
-    estimator = Estimator()
-    z_op = Pauli("ZIII")
-    grad = np.zeros_like(params)
-    print('Gradient calculation')
-    for _, x in enumerate(batch):
-        qc = GQHAN(x)
-        gradient = ParamShiftEstimatorGradient(estimator)
-        pse_grad_result = gradient.run(qc, z_op, [params.tolist()]).result().gradients
-        g = np.array(pse_grad_result)[0]
-        grad += g
-        
-    return grad/len(batch)
+#def nesterov(train_data, train_labels, init_params, learning_rate=0.001, momentum=0.9, epochs=4, batch_size=30):
+#
+#    params = init_params
+#    velocity = np.zeros_like(params)
+#
+#    for epoch in range(epochs):
+#        print(f'EPOCH NUMBER {epoch}')
+#        indices = np.random.permutation(len(train_data))
+#        train_data, train_labels = train_data[indices], train_labels[indices]
+#        loss_values = []
+#
+#        for i in range(0, len(train_data), batch_size):
+#            print(i)
+#            batch = train_data[i : i + batch_size]
+#            batch_labels = train_labels[i : i + batch_size]            
+#            lookahead_params = params - momentum * velocity
+#
+#            loss, dL_df = cost_function(lookahead_params, batch, batch_labels)
+#            loss_gradient = compute_gradient(lookahead_params, batch, dL_df)
+#            velocity = momentum * velocity + learning_rate * loss_gradient
+#            params -= velocity
+#            
+#            if i%60 == 0:
+#                print(f'loss at iteration {i} is {loss:.4f}')
+#                loss_values.append(loss)
+#        print('Evaluating test accuracy')
+#        acc = accuracy(params, test_images_pca, test_labels)
+#        print(f'test accuracy at epoch {epoch}: {acc:.2f}')
+#
+#    return params, loss_values, acc
 
-def nesterov(train_data, train_labels, init_params, learning_rate=0.09, momentum=0.9, epochs=4, batch_size=30):
-    params = init_params
-    velocity = np.zeros_like(params)
+#np.random.seed(42)
+#init_params = np.random.uniform(0, 2*np.pi, 14)
+#init_params = init_params.astype(np.float64)
+#optimized_params, loss, acc = nesterov(train_images_pca, train_labels, init_params)
+#save_dir = os.makedirs('parameters', exist_ok=True)
+#save_path = os.path.join(os.getcwd(), save_dir, "optimized_params.npy")
+#np.save(save_path, optimized_params)
 
-    for epoch in range(epochs):
-        print(f'EPOCH NUMBER {epoch}')
-        indices = np.random.permutation(len(train_data))
-        train_data, train_labels = train_data[indices], train_labels[indices]
-        loss_values = []
 
-        for i in range(0, len(train_data), batch_size):
-            print(i)
-            batch = train_data[i : i + batch_size]
-            batch_labels = train_labels[i : i + batch_size]            
-            lookahead_params = params - momentum * velocity
+from qiskit_machine_learning.circuit.library import RawFeatureVector
+from qiskit.circuit.library import StatePreparation
+from quantum_NeuralNet import ansatz
 
-            ##COMPUTE GRADIENT OF LOSS FUNCTION WITH RESPECT TO QC PARAMETER, FIRST CONSIDER THE CHAIN RULE
-            ##THEN USE PARAMETER SHIFT RULE FOR THE QUANTUM CIRCUIT
-            loss, dL_df = cost_function(lookahead_params, batch, batch_labels)
-            gradient = compute_gradient(lookahead_params, batch)
-            loss_gradient = dL_df*gradient
-
-            velocity = momentum * velocity + learning_rate * loss_gradient
-            params -= velocity
-            
-            if i%60 == 0:
-                print(f'loss at iteration {i} is {loss:.4f}')
-                loss_values.append(loss)
-        print('Evaluating test accuracy')
-        acc = accuracy(params, test_images_pca, test_labels)
-        print(f'test accuracy at epoch {epoch}: {acc:.2f}')
-
-    return params, loss_values, acc
-
-np.random.seed(42)
+qc = QuantumCircuit(4)
+amplitude = RawFeatureVector(8)
+qc.append(amplitude, [1, 2, 3])
+qc = ansatz(qc)
+print(qc.parameters)
+print(qc.draw('text'))
+x = train_images_pca[0]
 init_params = np.random.uniform(0, 2*np.pi, 14)
 init_params = init_params.astype(np.float64)
-optimized_params, loss = nesterov(train_images_pca, train_labels, init_params)
+z_op = Pauli("ZIII")
+estimator_qnn = EstimatorQNN(circuit= qc,
+                             #estimator = estimator,
+                             input_params = qc.parameters[14:],
+                             weight_params= qc.parameters[0:14])
 
-save_dir = os.makedirs('parameters', exist_ok=True)
-save_path = os.path.join(os.getcwd(), save_dir, "optimized_params.npy")
-np.save(save_path, optimized_params)
+
+
+x = train_images_pca[0:30]
+x = x/np.linalg.norm(x)
+p1 = estimator_qnn.forward(input_data=x, weights=init_params)
+estimator_qnn.backward(input_data = x, weights = init_params)
